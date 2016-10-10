dbc60918a5cdb451278d99168a2360cf5f37c547
Jaroslav Beran <32241174+berk76@users.noreply.github.com>
2016-10-10 20:28:30 +0200

    Initial import

---
+++ src/main/java/cz/webstones/words/AudioFilePlayer.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.webstones.words;
+
+
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import static javax.sound.sampled.AudioSystem.getAudioInputStream;
+import static javax.sound.sampled.AudioFormat.Encoding.PCM_SIGNED;
+import javax.sound.sampled.DataLine.Info;
+
+/**
+ *
+ * @author jaroslav_b
+ */
+public class AudioFilePlayer {
+    
+    public static void playFile(String path) {
+        final File file = new File(path);
+
+        try (final AudioInputStream in = getAudioInputStream(file)) {
+
+            final AudioFormat outFormat = getOutFormat(in.getFormat());
+            final Info info = new Info(SourceDataLine.class, outFormat);
+
+            try (final SourceDataLine line =
+                     (SourceDataLine) AudioSystem.getLine(info)) {
+
+                if (line != null) {
+                    line.open(outFormat);
+                    line.start();
+                    stream(getAudioInputStream(outFormat, in), line);
+                    line.drain();
+                    line.stop();
+                }
+            }
+
+        } catch (UnsupportedAudioFileException 
+               | LineUnavailableException 
+               | IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+    
+    private static AudioFormat getOutFormat(AudioFormat inFormat) {
+        final int ch = inFormat.getChannels();
+
+        final float rate = inFormat.getSampleRate();
+        return new AudioFormat(PCM_SIGNED, rate, 16, ch, ch * 2, rate, false);
+    }
+
+    private static void stream(AudioInputStream in, SourceDataLine line) 
+        throws IOException {
+        final byte[] buffer = new byte[4096];
+        for (int n = 0; n != -1; n = in.read(buffer, 0, buffer.length)) {
+            line.write(buffer, 0, n);
+        }
+    }
+}
---
+++ src/main/java/cz/webstones/words/WordDto.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.webstones.words;
+
+/**
+ *
+ * @author jaroslav_b
+ */
+public class WordDto {
+    private String cz;
+    private String en;
+
+    /**
+     * @return the cz
+     */
+    public String getCz() {
+        return cz;
+    }
+
+    /**
+     * @param cz the cz to set
+     */
+    public void setCz(String cz) {
+        this.cz = cz;
+    }
+
+    /**
+     * @return the en
+     */
+    public String getEn() {
+        return en;
+    }
+
+    /**
+     * @param en the en to set
+     */
+    public void setEn(String en) {
+        this.en = en;
+    }
+}
