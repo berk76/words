181a33139bdd92af3cdc319399d544faea6346ee
Jaroslav Beran <jaroslav.beran@gmail.com>
2020-11-01 22:32:58 +0100

    Version

--- src/main/java/cz/webstones/words/Service.java
+++ src/main/java/cz/webstones/words/Service.java
@@ -32,7 +32,7 @@ public class Service {
     
     private static final Logger LOGGER = Logger.getLogger(Service.class.getName());
 
-    public static final String VERSION = "Words 1.10.0 snapshot";
+    public static final String VERSION = "Words 1.11.0 snapshot";
     public static final String SETUP_FNAME = "setup.properties";
     private static final String HISTORY_FNAME = "history.properties";
     private static final String APP_DATA_DIR = "WordsData";
