f340038a8bf6847e91ce5f9807f00657b39caa32
Jaroslav Beran <jaroslav.beran@gmail.com>
2021-04-02 21:13:27 +0200

    About dialog

--- src/main/java/cz/webstones/words/AboutDialog.java
+++ src/main/java/cz/webstones/words/AboutDialog.java
@@ -1,7 +1,16 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+*       AboutDialog.java
+*
+*       This file is part of Words project.
+*       https://github.com/berk76/words
+*
+*       Words is free software; you can redistribute it and/or modify
+*       it under the terms of the GNU General Public License as published by
+*       the Free Software Foundation; either version 3 of the License, or
+*       (at your option) any later version. <http://www.gnu.org/licenses/>
+*
+*       Written by Jaroslav Beran <jaroslav.beran@gmail.com>
+*/
 package cz.webstones.words;
 
 import cz.webstones.words.dictionary.DictionaryStateEnum;
@@ -17,6 +26,9 @@ import java.net.URISyntaxException;
 import cz.webstones.words.dictionary.IDictionary;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.GroupLayout;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 /**
  *
@@ -24,7 +36,20 @@ import java.util.logging.Logger;
  */
 public class AboutDialog extends JEscapeableDialog implements IObserver {
     
+    private static final long serialVersionUID = -2183131500093783064L;
     private static final Logger LOGGER = Logger.getLogger(AboutDialog.class.getName());
+    
+    private javax.swing.JButton btnClose;
+    private javax.swing.JLabel lblVersion;
+    private javax.swing.JLabel lblWrittenBy;
+    private javax.swing.JLabel lblWeb;
+    private javax.swing.JLabel lblWebLink;
+    private javax.swing.JLabel lblJvmVendor;
+    private javax.swing.JLabel lblJvmVersion;
+    private javax.swing.JLabel lblDictionary;
+    private javax.swing.JLabel lblLang;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
     
     private static final String URL_GITHUB = "https://berk76.github.io/words/";
     private IDictionary dict;
@@ -36,17 +61,76 @@ public class AboutDialog extends JEscapeableDialog implements IObserver {
     public AboutDialog(java.awt.Frame parent, boolean modal, IDictionary dict) {
         super(parent, modal);
         initComponents();
-        this.setLocationRelativeTo(null);
         
         this.dict = dict;
         dict.attach(this);
         errorDialog = new ErrorDialog(parent, true);
+    }
+    
+    @Override
+    public void updateObserver() {
+        if (dict.getSubjectState() == DictionaryStateEnum.DICTIONARY_LOADED) {
+            try {
+                setPronunciation(dict.getLanguage());
+            } catch (IOException ex) {
+                errorDialog.showError("Error: Cannot download pronunciation.", ex);
+            }
+            setDictionaryName(dict.getDictionaryName());
+        }
+    }
 
-        jLabel1.setText(Service.VERSION);
-        jLabel4.setText(URL_GITHUB);
-        jLabel4.setForeground(Color.BLUE.darker());
-        jLabel4.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        jLabel4.addMouseListener(new MouseAdapter() {
+    private void setPronunciation(LanguageDto lang) {
+        if (lang != null) {
+            String p = "Pronunciation: %s [%s]";
+            lblLang.setText(String.format(p, lang.getName(), lang.getCode()));
+        }
+    }
+    
+    private void setDictionaryName(String dictName) {
+        if (dictName != null) {
+            lblDictionary.setText(String.format("Dictionary: %s", dictName));
+        }
+    }
+    
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
+        this.setVisible(false);
+    }
+
+    private void initComponents() {
+        lblVersion = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        lblWrittenBy = new javax.swing.JLabel();
+        lblWeb = new javax.swing.JLabel();
+        btnClose = new javax.swing.JButton();
+        lblWebLink = new javax.swing.JLabel();
+        lblJvmVendor = new javax.swing.JLabel();
+        lblJvmVersion = new javax.swing.JLabel();
+        lblDictionary = new javax.swing.JLabel();
+        lblLang = new javax.swing.JLabel();
+
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("About");
+
+        lblVersion.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        lblVersion.setText(Service.VERSION);
+
+        jTextArea1.setEditable(false);
+        jTextArea1.setColumns(20);
+        jTextArea1.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        jTextArea1.setLineWrap(true);
+        jTextArea1.setRows(5);
+        jTextArea1.setText("Words is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version. <http://www.gnu.org/licenses/>");
+        jTextArea1.setWrapStyleWord(true);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        lblWrittenBy.setText("Written by Jaroslav Beran");
+
+        lblWebLink.setText(URL_GITHUB);
+        lblWebLink.setForeground(Color.BLUE.darker());
+        lblWebLink.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        lblWebLink.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 try {
@@ -66,163 +150,70 @@ public class AboutDialog extends JEscapeableDialog implements IObserver {
                 // the mouse has exited the label
             }
         });
-        
-        jLabel5.setText("JVM Vendor: " + System.getProperty("java.vm.vendor"));
-        jLabel6.setText("JVM Version: " + System.getProperty("java.version"));
-    }
-    
-    @Override
-    public void updateObserver() {
-        if (dict.getSubjectState() == DictionaryStateEnum.DICTIONARY_LOADED) {
-            try {
-                setPronunciation(dict.getLanguage());
-            } catch (IOException ex) {
-                errorDialog.showError("Error: Cannot download pronunciation.", ex);
-            }
-            setDictionaryName(dict.getDictionaryName());
-        }
-    }
 
-    private void setPronunciation(LanguageDto lang) {
-        if (lang != null) {
-            String p = "Pronunciation: %s [%s]";
-            jLabel8.setText(String.format(p, lang.getName(), lang.getCode()));
-        }
-    }
-    
-    private void setDictionaryName(String dictName) {
-        if (dictName != null) {
-            jLabel7.setText(String.format("Dictionary: %s", dictName));
-        }
-    }
-
-    /**
-     * This method is called from within the constructor to initialize the form.
-     * WARNING: Do NOT modify this code. The content of this method is always
-     * regenerated by the Form Editor.
-     */
-    @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
-
-        jLabel1 = new javax.swing.JLabel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        jLabel8 = new javax.swing.JLabel();
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        setTitle("About");
-
-        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        jLabel1.setText("Words");
-
-        jTextArea1.setEditable(false);
-        jTextArea1.setColumns(20);
-        jTextArea1.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jTextArea1.setLineWrap(true);
-        jTextArea1.setRows(5);
-        jTextArea1.setText("Words is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version. <http://www.gnu.org/licenses/>");
-        jTextArea1.setWrapStyleWord(true);
-        jScrollPane1.setViewportView(jTextArea1);
-
-        jLabel2.setText("Written by Jaroslav Beran");
-
-        jLabel3.setText("Web:");
-
-        jButton1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jButton1.setText("Close");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnClose.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        btnClose.setText("Close");
+        btnClose.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
 
-        jLabel4.setText("jLabel4");
-
-        jLabel5.setText("jLabel5");
-
-        jLabel6.setText("jLabel6");
-
-        jLabel7.setText("jLabel7");
-
-        jLabel8.setText("jLabel8");
+        lblJvmVendor.setText("JVM Vendor: " + System.getProperty("java.vm.vendor"));
+        lblJvmVendor.setText("jLabel5");
+        lblJvmVersion.setText("JVM Version: " + System.getProperty("java.version"));
+        lblDictionary.setText("Dictionary:");
+        lblLang.setText("Language:");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 321, Short.MAX_VALUE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(jButton1))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel3)
-                        .addGap(18, 18, 18)
-                        .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel5)
-                            .addComponent(jLabel6)
-                            .addComponent(jLabel7)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel8))
-                        .addGap(0, 0, Short.MAX_VALUE)))
-                .addContainerGap())
+            layout.createParallelGroup(Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addContainerGap()
+                    .addGroup(layout.createParallelGroup(Alignment.LEADING)
+                        .addComponent(lblVersion, GroupLayout.DEFAULT_SIZE, 321, Short.MAX_VALUE)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(lblWeb)
+                            .addGap(18)
+                            .addComponent(lblWebLink, GroupLayout.DEFAULT_SIZE, 272, Short.MAX_VALUE))
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(lblWrittenBy)
+                            .addGap(39, 175, Short.MAX_VALUE))
+                        .addComponent(lblJvmVendor)
+                        .addComponent(lblJvmVersion)
+                        .addComponent(lblDictionary)
+                        .addComponent(lblLang)
+                        .addComponent(jScrollPane1, GroupLayout.DEFAULT_SIZE, 321, Short.MAX_VALUE)
+                        .addComponent(btnClose, Alignment.TRAILING))
+                    .addContainerGap())
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel4))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 25, Short.MAX_VALUE)
-                .addComponent(jLabel5)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel6)
-                .addGap(4, 4, 4)
-                .addComponent(jLabel7)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel8)
-                .addGap(16, 16, 16)
-                .addComponent(jButton1)
-                .addContainerGap())
+            layout.createParallelGroup(Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(lblVersion, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE)
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addComponent(jScrollPane1, GroupLayout.PREFERRED_SIZE, 106, GroupLayout.PREFERRED_SIZE)
+                    .addGap(18)
+                    .addComponent(lblWrittenBy)
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addGroup(layout.createParallelGroup(Alignment.BASELINE)
+                        .addComponent(lblWeb)
+                        .addComponent(lblWebLink))
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addComponent(lblJvmVendor)
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addComponent(lblJvmVersion)
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addComponent(lblDictionary)
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addComponent(lblLang)
+                    .addPreferredGap(ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
+                    .addComponent(btnClose)
+                    .addContainerGap())
         );
+        getContentPane().setLayout(layout);
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        this.setVisible(false);
-    }//GEN-LAST:event_jButton1ActionPerformed
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea jTextArea1;
-    // End of variables declaration//GEN-END:variables
+    }
 }
